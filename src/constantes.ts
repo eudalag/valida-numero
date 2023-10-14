021ed46a9877637bb564e296b0c8bc354e924ae9
eudalag <eudalag@gmail.com>
2023-10-14 10:55:36 -0400

    cambio4

--- src/constantes.ts
+++ src/constantes.ts
@@ -1,2 +1,3 @@
 //export const positiveNumberRegex = /^([0-9]+\.?[0-9]*)?$/;
-export const positiveNumberRegex = /^\d*\.?\d+$/;
+//export const positiveNumberRegex = /^\d*\.?\d+$/;
+export const positiveNumberRegex = /^\d*+\.?\d+$/;
