bbec44d905d89ebb09138d555072f32e6f9d5cd2
eudalag <eudalag@gmail.com>
2023-10-14 10:13:14 -0400

    inicio commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const handleChangeDemandLimit = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { value } = event.target as HTMLInputElement;
+    // Expresión regular para aceptar números enteros o decimales
+    if (/^\d*\.?\d*$/.test(value)) {
+      setInputValue(value);
+      console.log("permite");
+    } else {
+      console.log("no permite");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        id="percentage-edit"
+        data-testid="percentage-edit"
+        value={inputValue}
+        onChange={(e) => handleChangeDemandLimit(e)}
+      />
     </div>
   );
 }
